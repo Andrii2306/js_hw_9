4150a0c2cea331ed7f6eab1cecb277b74393616e
Andrii2306 <andriy06b@gmail.com>
2022-10-24 22:13:40 +0300

    9_3

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,21 +1,17 @@
 import flatpickr from 'flatpickr';
 // Додатковий імпорт стилів
 import 'flatpickr/dist/flatpickr.min.css';
-
 import Notiflix from 'notiflix';
 
 const selectedDates = document.querySelector('input#datetime-picker');
-
 const startCounterBtn = document.querySelector('[data-start]');
 startCounterBtn.disabled = true;
-
 const counterDays = document.querySelector('[data-days]');
 const counterHours = document.querySelector('[data-hours]');
 const counterMinutes = document.querySelector('[data-minutes]');
 const counterSeconds = document.querySelector('[data-seconds]');
 
-let ms; // змінна для запису часу відліку у мс
-
+let ms;
 let timerId = null;
 
 const options = {
@@ -25,8 +21,6 @@ const options = {
   minuteIncrement: 1,
   onClose(selectedDates) {
     let currentDate = new Date();
-
-    //перевірка дати на валідність (майбутнє):
     if (selectedDates[0] < currentDate) {
       startCounterBtn.disabled = true;
 
@@ -36,46 +30,27 @@ const options = {
       counterMinutes.textContent = '00';
       counterSeconds.textContent = '00';
 
-      //alert("Please choose a date in the future");
-      Notiflix.Report.failure(
-        'Wrong date!',
-        'Please choose a date in the future',
-        'Ok'
-      );
-
+      Notiflix.Report.failure('Please choose a date in the future');
       setTimeout(() => {
         document.location.reload();
-      }, 4000);
-
+      }, 1000);
       return;
     }
-
     console.log('currentDate: ', currentDate);
     console.log('selectedDates[0] : ', selectedDates[0]);
-
-    // активація кнопки
     startCounterBtn.disabled = false;
-
-    //слухач на кнопку
     startCounterBtn.addEventListener('click', onClick);
   },
 };
 
 const dataPickr = new flatpickr(selectedDates, options);
-
 function onClick() {
   timerId = setInterval(() => {
     let currentDate = new Date();
-
-    // визначення та запис у змінну часу відліку у мс
     ms = dataPickr.selectedDates[0] - currentDate;
-
     console.log('time to count, ms =', ms);
-
     const timeForCounter = convertMs(ms);
-
     console.log("It's timeForCounter inside setInterval :", timeForCounter);
-
     counterDays.textContent = addLeadingZero(timeForCounter.days, 0);
     counterHours.textContent = addLeadingZero(timeForCounter.hours, 0);
     counterMinutes.textContent = addLeadingZero(timeForCounter.minutes, 0);
@@ -98,7 +73,6 @@ function onClick() {
   }, 1000);
 }
 
-// Для підрахунку значень використовуй готову функцію convertMs, де ms - різниця між кінцевою і поточною датою в мілісекундах.
 function convertMs(ms) {
   // Number of milliseconds per unit of time
   const second = 1000;
