70c00de88f496b15617eea3af5cbe96f51e7304d
Andrii2306 <105624686+Andrii2306@users.noreply.github.com>
2022-10-24 18:44:25 +0000

    Deploying to gh-pages from @ Andrii2306/js_hw_9@9a777144518067924c1845409dfd7812c0653c17 🚀

---
+++ 01-color-switcher.3af8a367.js
@@ -0,0 +1,2 @@
+!function(){var t,n=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");n.addEventListener("click",(function(){t||(t=setInterval((function(){document.body.style.background="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3))})),e.addEventListener("click",(function(){clearInterval(t),t=null}))}();
+//# sourceMappingURL=01-color-switcher.3af8a367.js.map
